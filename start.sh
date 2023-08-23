33cd4ddf02c60a2a6088e21e2018cb9670d88616
qtvsprofff <142015253+qtvsprofff@users.noreply.github.com>
2023-08-23 17:08:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/qtvsprofff/Dq-filter-clone /Dq-filter-clone
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /Dq-filter-clone
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Dq-filter-clone...."
 python3 bot.py
